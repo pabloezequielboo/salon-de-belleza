5012822eae64eee0d2490cb2f814009cdeb1aec0
pabloezequielboo <pabloezequielboo@gmail.com>
2025-11-19 00:59:19 -0300

    Add files via upload

---
+++ proyectopablo/salon-de-belleza/core/migrations/__init__.py
@@ -0,0 +1,4 @@
+# Este archivo vacío le indica a Python que el directorio 'migrations'
+# debe ser considerado un paquete de Python.
+# Django almacena aquí los archivos de migración de la base de datos,
+# que son generados automáticamente cuando modificas tus modelos.
