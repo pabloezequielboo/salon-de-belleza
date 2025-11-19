788237d8d3282f9c455609725cf0ea4277dbf2ff
pabloezequielboo <pabloezequielboo@gmail.com>
2025-11-19 02:27:43 -0300

    Delete proyectopablo/salon-de-belleza directory

--- proyectopablo/salon-de-belleza/core/__init__.py
+++
@@ -1,2 +0,0 @@
-# Este archivo vacío le indica a Python que este directorio debe ser considerado un paquete de Python.
-# Es una convención de Python para los paquetes.
--- proyectopablo/salon-de-belleza/core/apps.py
+++
@@ -1,9 +0,0 @@
-# Este archivo se utiliza para configurar la aplicación 'core'.
-# Por ejemplo, puedes definir el nombre de la aplicación.
-
-from django.apps import AppConfig
-
-
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
